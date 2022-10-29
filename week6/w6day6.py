c57d9ae80aadb8f54a2a953263cfa9a83381ab2b
james <jamesmaloba94@gmail.com>
2022-10-29 22:23:27 +0300

    updated the question day6

--- week6/w6day6.py
+++ week6/w6day6.py
@@ -1,11 +1,11 @@
 # Problem
 # Chef's professor is planning to give his class a group assignment. 
-# There are 2N2N students in the class, with distinct roll numbers ranging from 11 to 2N2N. 
-# Chef's roll number is XX.
+# There are 2N students in the class, with distinct roll numbers ranging from 1 to 2N. 
+# Chef's roll number is X.
 
-# The professor decided to create NN groups of 22 students each. 
-# The groups were created as follows: the first group consists of roll numbers 11 and 2N2N, 
-# the second group of roll numbers 22 and 2N - 12N−1, and so on, with the final group consisting of roll numbers NN and N+1N+1.
+# The professor decided to create N groups of 2 students each. 
+# The groups were created as follows: the first group consists of roll numbers 1 and 2N, 
+# the second group of roll numbers 2 and 2N - 1, and so on, with the final group consisting of roll numbers N and N+1.
 
 # Chef wonders who his partner will be. Can you help Chef by telling him the roll number of his partner?
 
